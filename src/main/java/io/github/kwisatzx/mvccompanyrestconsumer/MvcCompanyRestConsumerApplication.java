66792ff6a855d78fa97a486785b42d81926b54be
KwisatzX <kwisatz4@gmail.com>
2021-12-17 05:22:49 +0100

    Initial commit

---
+++ src/main/java/io/github/kwisatzx/mvccompanyrestconsumer/MvcCompanyRestConsumerApplication.java
@@ -0,0 +1,13 @@
+package io.github.kwisatzx.mvccompanyrestconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcCompanyRestConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcCompanyRestConsumerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/kwisatzx/mvccompanyrestconsumer/MvcCompanyRestConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kwisatzx.mvccompanyrestconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcCompanyRestConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
